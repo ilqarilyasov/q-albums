5df4668e8f9525cf34420d993dd95e8f6feb46de
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-02-18 14:38:16 -0600

    Set up storyboard scenes, their classes. Add all outlet/action connections

---
+++ Qalbums/Views/SongTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SongTableViewCell.swift
+//  Qalbums
+//
+//  Created by Ilgar Ilyasov on 2/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+    
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var songTitleTextField: UITextField!
+    @IBOutlet weak var durationTextField: UITextField!
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func addSongButtonTapped(_ sender: Any) {
+        
+    }
+    
+}
