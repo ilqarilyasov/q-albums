3a2774fffa01d805bfd8da2134f6b0704a6618f5
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-02-18 16:58:26 -0600

    Add Networking functions for Get and Put

--- Qalbums/Model Controller/AlbumController.swift
+++ Qalbums/Model Controller/AlbumController.swift
@@ -10,6 +10,76 @@ import Foundation
 
 class AlbumController {
     
+    // MARK: - Properties
+    
+    private(set) var albums: [Album] = []
+    private let baseURL = URL(string: "https://journal-coredata-b5a96.firebaseio.com/")!
+    
+    
+    // MARK: - Networking
+    
+    func getAlbums(completion: @escaping (Error?) -> Void = { _ in }) {
+        let url = baseURL.appendingPathExtension("json")
+        
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            if let error = error {
+                NSLog("Error performing GET data task: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from URLSession")
+                completion(NSError())
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let jsonDict = try decoder.decode([String: Album].self, from: data)
+                self.albums = jsonDict.map{ $0.value }
+                completion(nil)
+                return
+            } catch {
+                NSLog("Error decoding firebase to Album: \(error)")
+                completion(error)
+            }
+        }
+    }
+    
+    
+    func put(album: Album) {
+        let idURL = baseURL.appendingPathComponent(album.id.uuidString)
+        let jsonURL = idURL.appendingPathExtension("json")
+        
+        var request = URLRequest(url: jsonURL)
+        request.httpMethod = "PUT"
+        
+        do {
+            let encoder = JSONEncoder()
+            let encodedAlbum = try encoder.encode(album)
+            request.httpBody = encodedAlbum
+        } catch {
+            NSLog("Error encoding Album to a JSON: \(error)")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            if let error = error {
+                NSLog("Error performing PUT data task: \(error)")
+                return
+            }
+            
+            if let response = response {
+                NSLog("URLSession PUT data task response: \(response)")
+                return
+            }
+        }
+    }
+    
+    
+    // MARK: - Testing
+    
     @discardableResult
     static func testDecodingExampleAlbum() -> Album? {
         guard let url = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json") else {
