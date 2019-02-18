b09388450b9ccf3374bcdb1e262311f6e3efa399
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-02-18 14:14:29 -0600

    Initial Commit

---
+++ Qalbums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Qalbums
+//
+//  Created by Ilgar Ilyasov on 2/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
